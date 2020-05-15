543069fa6fe46965ca0286278c32aeda0cc1b512
Coppelia <mfreese@gmx.ch>
2020-05-15 09:35:01 +0200

    simAddLog only when CoppeliaSim lib is loaded!

--- simExtReflexxesTypeII/simExtReflexxesTypeII.cpp
+++ simExtReflexxesTypeII/simExtReflexxesTypeII.cpp
@@ -65,12 +65,12 @@ SIM_DLLEXPORT unsigned char simStart(void* reservedPointer,int reservedInt)
 	simLib=loadSimLibrary(temp.c_str());
 	if (simLib==NULL)
 	{
-        simAddLog("ReflexxesTypeII",sim_verbosity_errors,"could not find or correctly load the CoppeliaSim library. Cannot start the plugin.");
+        printf("simExtReflexxesTypeII: error: could not find or correctly load the CoppeliaSim library. Cannot start the plugin.\n"); // cannot use simAddLog here.
         return(0); // Means error, CoppeliaSim will unload this plugin
 	}
 	if (getSimProcAddresses(simLib)==0)
 	{
-        simAddLog("ReflexxesTypeII",sim_verbosity_errors,"could not find all required functions in the CoppeliaSim library. Cannot start the plugin.");
+        printf("simExtReflexxesTypeII: error: could not find all required functions in the CoppeliaSim library. Cannot start the plugin.\n"); // cannot use simAddLog here.
 		unloadSimLibrary(simLib);
         return(0); // Means error, CoppeliaSim will unload this plugin
 	}
